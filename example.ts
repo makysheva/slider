1e0d9fa54045b5652ad3d303a3a0a9c2616bb171
vovnet <vovnet@yandex.ru>
2019-12-07 12:35:31 +0300

    Updates

--- example.ts
+++
@@ -1,7 +0,0 @@
-import './scss/example.scss';
-import { Orientation } from "../model/Orientation";
-import { Panel } from './panel/Panel';
-
-$(function() {
-    new Panel(document.body, {orientation: Orientation.Vertical, showLabels: true, range: true, step: 4});
-});
