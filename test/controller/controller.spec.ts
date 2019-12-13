cfa61bd22d6bf83fb616a19d2cf69983fbf9b324
vovnet <vovnet@yandex.ru>
2019-12-13 16:47:43 +0300

    Added test coverage lib

--- test/controller/controller.spec.ts
+++ test/controller/controller.spec.ts
@@ -1,7 +1,17 @@
 import 'mocha';
 import { expect, assert } from 'chai';
-import sinon, { SinonSpy } from 'sinon';
+import sinon, { SinonSpy, SinonStub } from 'sinon';
+import { SliderModel } from '../../src/model/SliderModel';
+import { Controller } from '../../src/controller/Controller';
+import { Orientation } from '../../src/model/Orientation';
 
 describe('Controller tests', () => {
-    
+    it('Change orientation', () => {
+        let model: SliderModel = new SliderModel({});
+        let controller: Controller = new Controller(document.body, model);
+        
+        controller.changeOrientation(Orientation.Vertical);
+        
+        expect(model.orientation).to.equal(Orientation.Vertical);     
+    });
 });
