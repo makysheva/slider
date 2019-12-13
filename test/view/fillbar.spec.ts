ca6d15844ddd474672ae9ddb86bdca62d8270951
vovnet <vovnet@yandex.ru>
2019-12-13 11:06:57 +0300

    Added test for update fillbar\'s method

--- test/view/fillbar.spec.ts
+++ test/view/fillbar.spec.ts
@@ -27,13 +27,24 @@ describe('FillBar tests', () => {
         expect(document.querySelectorAll('.slider__fill--vertical').length).to.equal(0);
     });
 
-    it('Update method', () => {
+    it('Update method with horizontal orientation', () => {
         let low: number = 10;
         let hight: number = 20;
         let fill: FillBar = new FillBar(document.body, Orientation.Horizontal);
         fill.update(low, hight, Orientation.Horizontal);
-        let element: HTMLElement = document.querySelector('.slider__fill');
-        expect(element.style.left).to.equal(low);
+        let element: HTMLElement = document.querySelector('.slider__fill') as HTMLElement;
+        expect(element.style.left).to.equal(low + 'px');
+        expect(element.style.right).to.equal(hight + 'px');
+    });
+
+    it('Update method with vertical orientation', () => {
+        let low: number = 10;
+        let hight: number = 20;
+        let fill: FillBar = new FillBar(document.body, Orientation.Vertical);
+        fill.update(low, hight, Orientation.Vertical);
+        let element: HTMLElement = document.querySelector('.slider__fill--vertical') as HTMLElement;
+        expect(element.style.bottom).to.equal(low + 'px');
+        expect(element.style.top).to.equal(hight + 'px');
     });
 
     beforeEach(() => document.body.innerHTML = '');
