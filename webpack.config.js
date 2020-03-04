ef9abc3ffb962126364152ee6df8b3d3dbff457c
vovnet <vovnet@yandex.ru>
2020-03-04 13:03:29 +0300

    #4 set index file by default

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
@@ -10,40 +10,41 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.pug$/,
-        use: 'pug-loader'
+        use: 'pug-loader',
       },
       {
         test: /\.s[ac]ss$/,
         use: [
           'style-loader',
           'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
+          'sass-loader',
+        ],
+      },
+    ],
   },
 
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: ['.tsx', '.ts', '.js'],
   },
 
   plugins: [
-    new htmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       template: 'example/index.pug',
-      filename: 'example/index.html'
+      filename: 'example/index.html',
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',
-      jQuery: 'jquery'
+      jQuery: 'jquery',
     }),
   ],
 
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
-    open: true
-  }
-}
+    open: true,
+    index: 'example/index.html',
+  },
+};
